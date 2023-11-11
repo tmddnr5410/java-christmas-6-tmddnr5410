98876985c7f05a43ff0af207b0b0ef7da145d2b3
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-11 13:18:19 +0900

    feat : 입력에 대한 예외처리 기능 추가

---
+++ src/main/java/christmas/Constant/ErrorMessage.java
@@ -0,0 +1,6 @@
+package christmas.Constant;
+
+public class ErrorMessage {
+    public static final String NOT_INT = "[ERROR] 입력은 숫자여야 합니다.";
+    public static final String NO_INPUT = "[ERROR] 입력이 없습니다.";
+}
--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,6 +1,7 @@
 package christmas.View;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.Constant.ErrorMessage;
 
 public class InputView {
     private final String INPUT_MESSAGE_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
@@ -9,8 +10,28 @@ public class InputView {
         System.out.println(INPUT_MESSAGE_DATE);
         String input = Console.readLine();
 
+        validate(input);
+
         return Integer.parseInt(input);
     }
 
+    private void validate(String input) {
+        exceptInputNone(input);
+        exceptInteger(input);
+    }
+
+    private void exceptInputNone(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.NO_INPUT);
+        }
+    }
+
+    private void exceptInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_INT);
+        }
+    }
 
 }
