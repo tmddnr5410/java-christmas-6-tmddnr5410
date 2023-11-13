3ee75da954e73796a1444e4f002fd68b3ede9a24
LeeSeungUk <tmddnr5410@gmail.com>
2023-11-13 19:33:49 +0900

    fix : 잘못 입력된 메뉴 가격 수정

--- src/main/java/christmas/Domain/Menu.java
+++ src/main/java/christmas/Domain/Menu.java
@@ -8,13 +8,14 @@ public enum Menu {
     TAPAS(MenuType.APPETIZER, "타파스", 5500),
     CAESAR_SALAD(MenuType.APPETIZER, "시저샐러드", 8000),
     T_BONE_STAKE(MenuType.MAIN, "티본스테이크", 55000),
-    BARBECUE_RIB(MenuType.MAIN, "바비큐립", 6000),
-    CAESAR_PASTA(MenuType.MAIN, "해산물파스타", 6000),
-    CHOCO_CAKE(MenuType.DESERT, "초코케이크", 6000),
-    ICE_CREAM(MenuType.DESERT, "아이스크림", 6000),
-    ZERO_COKE(MenuType.DRINK, "제로콜라", 6000),
-    RED_WINE(MenuType.DRINK, "레드와인", 6000),
-    CHAMPAGNE(MenuType.DRINK, "샴페인", 6000);
+    BARBECUE_RIB(MenuType.MAIN, "바비큐립", 54000),
+    CAESAR_PASTA(MenuType.MAIN, "해산물파스타", 35000),
+    CHRISTMAS_PASTA(MenuType.MAIN, "크리스마스파스타", 45000),
+    CHOCO_CAKE(MenuType.DESERT, "초코케이크", 15000),
+    ICE_CREAM(MenuType.DESERT, "아이스크림", 5000),
+    ZERO_COKE(MenuType.DRINK, "제로콜라", 3000),
+    RED_WINE(MenuType.DRINK, "레드와인", 60000),
+    CHAMPAGNE(MenuType.DRINK, "샴페인", 25000);
 
 
     private final String type;
@@ -33,6 +34,10 @@ public enum Menu {
 
     public String getName() {
         return name;
+    }
+
+    public Integer getPrice() {
+        return price;
     }
 
     public static boolean isNotInMenu(String orderMenu) {
